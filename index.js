a338ebf8bee5f97f58a90c961b9f7d15086f9d89
happyso <cheza1220@gmail.com>
2020-02-20 21:19:27 +0900

    challenge code complete

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
+const app = express();
 const PORT = 4000;
 
-function handleListening() {
-  console.log(`Listening on: http://localhost:${PORT}`);
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req,res) => res.send("Hello from Home");
+const handleProfile = (req, res) => res.send("You are on my profile");
+const handleAboutus = (req, res) => res.send("About us");
+const handleContact = (req, res) => res.send("Contact");
+const handleProtected = (req, res) => res.send("Protected!!");
+  
+const betweenAll = (req, res, next) => {
+    console.log("I'm a middleware");
+    next();
+};
+  
+const middleware = (req, res, next) => {
+    res.redirect('/');
+};
+app.use(betweenAll);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.get("/about-us", handleAboutus);
+app.get("/contact", handleContact);
+app.get("/protected",middleware, handleProtected);
 
 app.listen(PORT, handleListening);
+
