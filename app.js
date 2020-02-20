b2e8934a0d84ac1d91c4fb146f028b6bfb5d9f1f
happyso <cheza1220@gmail.com>
2020-02-20 21:42:35 +0900

    add router

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req,res) => res.send("Hello from Home");
 const handleProfile = (req, res) => res.send("You are on my profile");
@@ -15,15 +13,6 @@ const handleAboutus = (req, res) => res.send("About us");
 const handleContact = (req, res) => res.send("Contact");
 const handleProtected = (req, res) => res.send("Protected!!");
   
-const betweenAll = (req, res, next) => {
-    console.log("I'm a middleware");
-    next();
-};
-  
-const middleware = (req, res, next) => {
-    res.redirect('/');
-};
-app.use(betweenAll);
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -32,10 +21,11 @@ app.use(helmet());
 app.use(morgan("dev"));
 
 app.get("/", handleHome);
+app.use("/user", userRouter);
 app.get("/profile", handleProfile);
 app.get("/about-us", handleAboutus);
 app.get("/contact", handleContact);
-app.get("/protected",middleware, handleProtected);
 
-app.listen(PORT, handleListening);
 
+
+export default app;
